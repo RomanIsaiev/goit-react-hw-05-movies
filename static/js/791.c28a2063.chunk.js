b3e31c0fd97925cadcc73146edca2b7222a36707
RomanIsaiev <107892291+RomanIsaiev@users.noreply.github.com>
2023-11-26 10:37:40 +0000

    Deploying to gh-pages from @ RomanIsaiev/goit-react-hw-05-movies@d9986f20d1f713388ccb247c2575cafe948d98a1 🚀

---
+++ static/js/791.c28a2063.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[791],{680:function(e,t,r){r.r(t),r.d(t,{default:function(){return a}});var n=r(87),o=r(184);function a(){return(0,o.jsx)("div",{children:(0,o.jsxs)("p",{children:["Sorry, page not found. Please use this ",(0,o.jsx)(n.rU,{to:"/",children:"link"})," to navigate to our home page."]})})}}}]);
+//# sourceMappingURL=791.c28a2063.chunk.js.map
