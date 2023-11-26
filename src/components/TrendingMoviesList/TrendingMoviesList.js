1113f0dcb5330322c70657d1804b371443e6d020
Roman Isaiev <roma.isaev.2000@gmail.com>
2023-11-26 13:10:31 +0200

    remove trash

--- src/components/TrendingMoviesList/TrendingMoviesList.js
+++ src/components/TrendingMoviesList/TrendingMoviesList.js
@@ -1,31 +1,10 @@
-import {
-  getMovieById,
-  getTrendingMovies,
-} from 'components/ApiService/ApiService';
+import { getTrendingMovies } from 'components/ApiService/ApiService';
 import { MoviesList } from 'pages/TopMoviesList';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
 
 export const TrendingMoviesList = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
   // const [isLoading, setIsLoading] = useState(false);
-
-  const [movie, setMovie] = useState(null);
-  const params = useParams();
-
-  useEffect(() => {
-    async function componentDidUpdate() {
-      try {
-        await getMovieById(params.movieId).then(response => {
-          setMovie(response.data);
-        });
-      } catch (error) {
-      } finally {
-      }
-    }
-
-    componentDidUpdate();
-  }, [params]);
 
   useEffect(() => {
     async function componentDidUpdate() {
@@ -44,20 +23,7 @@ export const TrendingMoviesList = () => {
 
   return (
     <div>
-      {movie ? (
-        <div>
-          <img
-            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-            alt={movie.title}
-            width={300}
-            height={400}
-          />
-          <h2>{movie.title}</h2>
-          <p>{movie.overview}</p>
-        </div>
-      ) : (
-        <MoviesList topMoviesList={trendingMovies} />
-      )}
+      <MoviesList topMoviesList={trendingMovies} />
     </div>
   );
 };
